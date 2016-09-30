b55a97add21ddff0546ac8b18a9c415ca01d199a
wangweijun <2219962313@qq.com>
2016-09-30 14:22:03 +0800

    添加了一个我的网页文件并且修改login.php

--- chat/login.php
+++ chat/login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -12,5 +12,8 @@
 <p>密码：<input type="password" value="" name="password" /></p>
 <p><input type="submit" name="submit" value="登陆 "/>&nbsp;&nbsp;<a href='regist.php' target='_blank'>注册</a></p>
 </form>
+<div>
+欢迎来到狗狗时代
+</div>
 </body>
 </html>
